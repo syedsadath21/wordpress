c07c3e3687d6689d7951056b4f233b3f06cb5853
Syed Sadath <35702238+syedsadath21@users.noreply.github.com>
2020-03-12 14:24:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
    var altText;   
    const cros = "https://cors-anywhere.herokuapp.com/";
 
-   if(document.getElementById("input").value.length === 7){
-    $('.submit').attr('disabled','true');
-  }
+//    if(document.getElementById("input").value.length === 7){
+//     $('.submit').attr('disabled','true');
+//   }
 
      //  $('.getUrls').click(function(event){
     function fetchData(){
       $('#data').empty();
       $('.submit').attr('disabled','true');
       $('#input').attr('disabled','true');      
-      $('#loading-point').html('<img class="loading" src="./images/source.gif" />');
+      $('#loading-point').html('<img class="loading" src="./images/blue.gif" />');
 
       
 
@@ -34,16 +34,19 @@
               .catch(erorStatus)
           ))
           
+        }
+        
           function erorStatus(response){
             if(response.ok !== false){
                 $('<h4 class="alert alert-danger hideEror">'+ "Please check the SubID and try again." + '</h4>').appendTo('#error-msg');
-            }   
+            } 
             var erMsg = document.getElementsByClassName('alert-danger') 
             if(erMsg.length > 1){          
                 $('<h4 class="alert alert-danger">&#10071; '+ erMsg[4].innerText + '</h4>').appendTo('#error-msg');
                 $('#loading-point').empty();
             }
           }
+
           function checkStatus(response) {
               if (response.ok) {
                 return Promise.resolve(response.text());
@@ -66,14 +69,13 @@
                 let myJson = JSON.parse(linkList.childNodes[1].childNodes[1].innerText);
 
                 let firmName = myJson.name;
-                let builderLink = myJson.url;
+                var builderLink = myJson.url;
                 let subId = builderLink.substr(8,7);
                 
-                $('<h1 class="firmName">' + firmName + '</h1>').appendTo('#data');
-                
+                $('<h1 class="firmName">' + firmName + '</h1>').appendTo('#data');                
                 $('<h4><span>Builder URL </span>: <a target="_blank" class="builderLink">'+ builderLink + '</a></h4>').appendTo('#data');
                 $('.builderLink').attr('href',builderLink);
-                $('<h4><span>SubID </span>: '+ subId + '</h4>').appendTo('#data');
+                $('<h4 class="subid"><span>Sub ID </span>: '+ subId + '</h4>').appendTo('#data');
                 //dashboard link
                 $('<a target="_blank" class="link-btn dashLink">Dashboard</a>').appendTo('#data');
                 $('.dashLink').attr('href',builderLink+'/wp-admin/');
@@ -83,11 +85,50 @@
                 if(builderLink.length !== 0){
                     $('#loading-point').empty();
                 }
-                $('#linkList').empty();                                          
+                $('#linkList').empty();   
+                fetchWld();
+                
+                function fetchWld(){
+                    const myLink = cros+builderLink;
+                    fetch(myLink) 
+                    .then(response => response.text())
+                    .then(contents =>  $('<p>'+ ($(contents).find('.flfooterbrand').html()) + '</p>').appendTo('#linkList'))
+                    .then(getWld)
+                    .catch(error => console.log('Could not find WLD Id'))                   
+                }
+                function getWld(){
+                    let wldLink = document.getElementById('linkList').querySelectorAll('a')[0].getAttribute('href');
+                    let wldId = wldLink.substr(wldLink.length-13,7);
+                    $('<h4><span>WLD ID </span>: '+ wldId + '</h4>').insertAfter('.subid');
+                    //wld link
+                    $('<a target="_blank" class="link-btn wldLink">WLD</a>').appendTo('#data');
+                    $('.wldLink').attr('href',wldLink);
+                }
             }
             // $('.submit').removeAttr('disabled');
             // $('#input').removeAttr('disabled');
+
+        function fetchLiveSite(){
+            const inputLink = cros+ document.getElementById("inputUrl").value +'/wp-json/'; 
+
+            fetch(inputLink) 
+            .then(checkStatus)                 
+            .then(parseJSON)
+            .then(getUrls)
+            .catch(error => $('<h4 class="alert alert-danger">'+ "&#10071; Please check the URL and try again." + '</h4>').appendTo('#error-msg'))
         }
+
+
+        $(document).ready(function(){
+            $("#radio1").click(function(){
+              $('#myform-firm').hide();
+              $('#myform-sub').show();
+            });
+            $("#radio2").click(function(){
+                $('#myform-sub').hide();
+                $('#myform-firm').show();
+              });
+          });
 
 
     // Example starter JavaScript for disabling form submissions if there are invalid fields
